5fb049770ee7a98bc0c7703d230153f0a90e4826
Sander Moolin <sandermoolin@gmail.com>
2017-12-22 16:10:24 -0500

    Setting up home page

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,12 +1,42 @@
 <template>
 
-    <div>
+    <header class="main-menu">
 
-        <header>
+        <nav>
             <router-link to="/"><h1>Sander Moolin</h1></router-link>
-            <p></p>
-        </header>
+            <ul>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/ld40">Games</a></li>
+            </ul>
+        </nav>
 
-    </div>
+    </header>
 
 </template>
+
+<style lang="scss">
+@import 'src/styles/vars';
+
+.main-menu {
+    font-size: 21px;
+    text-align: left;
+    display: inline-block;
+    width: 350px;
+    margin-left: 0;
+    border: 4px solid #000;
+    padding: 0 30px;
+
+    position: absolute;
+    right: $body-padding;
+    bottom: $body-padding;
+
+    h1 {
+        border-bottom: 2px solid $color2;;
+    }
+    ul {
+        padding: 0;
+        list-style-type: none;
+    }
+}
+
+</style>
